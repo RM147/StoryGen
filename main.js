f644d6883d49e56ab1cd13cf31ab39a253549b60
RM147 <mittrarana@gmail.com>
2019-02-11 21:24:03 +0000

    update

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 'use strict';
 var x = "";
 var y = "";
-var JSONObject = "{\"word\": "+x+", \"genre\": "+y+"}";
+var noun = "noun";
+var verb = "verb";
+var adjective = "adjective";
+var JSONObject = "{\"word\": \"word\", \"type\": "+x+", \"genre\": "+y+"}";
+
+function printWords(word){
+    x = word;
+    console.log(JSONObject);
+}
