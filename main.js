da976840542079c18d4d33661fc7849f649bcb1e
RM147 <mittrarana@gmail.com>
2019-02-12 10:49:14 +0000

    things

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 'use strict';
 var x = "";
 var y = "";
-var noun = "noun";
-var verb = "verb";
-var adjective = "adjective";
+
 var JSONObject = "{\"word\": \"word\", \"type\": "+x+", \"genre\": "+y+"}";
 
-function printWords(word){
+function printWords(word){    
     x = word;
+    var JSONObject = "{\"word\": \"word\", \"type\": \""+x+"\", \"genre\": "+y+"}";
     console.log(JSONObject);
 }
