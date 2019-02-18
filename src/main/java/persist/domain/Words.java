be6932d23611d54284481e81750d5d5d912182d7
RM147 <mittrarana@gmail.com>
2019-02-18 15:12:53 +0000

    better database

--- src/main/java/persist/domain/Words.java
+++ src/main/java/persist/domain/Words.java
@@ -7,48 +7,54 @@ import javax.persistence.Id;
 
 @Entity
 public class Words {
-	
+
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Long id;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	private String type;
 	private String word;
 	private String genre;
-	
+
 	public Words() {
-		
 	}
-	
+
 	public Words(String word, String type, String genre) {
 		this.word = word;
-		this.type =  type;
+		this.type = type;
 		this.genre = genre;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getWord() {
 		return word;
 	}
+
 	public void setWord(String word) {
 		this.word = word;
 	}
+
 	public String getGenre() {
 		return genre;
 	}
+
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
-			
+
 }
