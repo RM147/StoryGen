020aa87685ab3141ea7e39fb10b9c13c5ac1ec5c
RM147 <mittrarana@gmail.com>
2019-02-22 13:19:21 +0000

    front end added

---
+++ Front End/frontend/src/GETbutton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class GETbutton extends Component {
+  render() {
+    return (
+      <p>Hi</p>
+      
+    );
+  }
+}
+
+export default GETbutton;
---
+++ src/test/java/tests/WordRepoTests.java
@@ -0,0 +1,43 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import util.JSONUtil;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WordRepoTests {
+	
+	@InjectMocks
+	private
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@Ignore
+	@Test
+	public void test() {
+		
+
+	}
+
+}
